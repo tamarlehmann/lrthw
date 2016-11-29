6f781f50527ce226a5d131012cd8cca3c83cf1b6
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-11-29 11:41:56 +0000

    Exercise 3

---
+++ ex3.rb
@@ -0,0 +1,36 @@
+#Note: PEMDAS Parentheses Exponents Multiplication Division Addition Subtraction
+
+# prints line of text
+puts "I will now count my chickens:"
+
+# prints and computes 25 + (30/6)
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# prints and computes 100 - (25 * 3 % 4)
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# prints line of text
+puts "Now I will count the eggs:"
+
+# prints result of sum below
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints line of text
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# prints boolean result of the expression, false
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# prints text with calculated sums inside the #{}
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# prints line of text
+puts "Oh, that's why it's false."
+
+# prints line of text
+puts "How about some more."
+
+# prints line of text followed by boolean as a result of comparison operators
+puts "Is it greater? #{5.0 > -2.0}" #true
+puts "Is it greater or equal? #{5.0 >= -2.0}" #true
+puts "Is it less or equal? #{5.0 <= -2.0}" #false
