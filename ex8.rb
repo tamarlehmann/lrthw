2c76a6f36bdd78a9945b01a25181bcb1d238929b
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-11-29 18:09:18 +0000

    Exercise 8

--- ex8.rb
+++ ex8.rb
@@ -1,10 +1,14 @@
+# define variable 'formatter' to a string containing 4 placeholders.
 formatter = "%{first} %{second} %{third} %{fourth}"
 
+# replaces placeholder with value specified for that placeholder eg. first -> 1
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
 puts formatter % {first: true, second: false, third: true, fourth: false}
+# replaces first with the string of formatter, will print formatter 4 times
 puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
 
+# puts out a sentence rather than string values, just in a list rather than lateral
 puts formatter % {
   first: "I had this thing.",
   second: "That you could type up right.",
