6216b2b7ecacad4bad4a09d3927b8ed57dfde77f
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-11-30 14:00:46 +0000

    Exercise 16

---
+++ ex16.rb
@@ -0,0 +1,41 @@
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbye"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+# this is a shorter way of writing the commands on one line
+target.write (line1 + "\n" + line2 + "\n" + line3 + "\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
+
+puts "And finally, we close it."
+target.close
+
+
+# Note that with open, the 'w' means open this file in write mode. Can use r for read
+# and a for append. Using open(filename) will open it in read mode.
+# When you open a file in write mode, truncate() is not necessary.
