50d2734f03c3a0477fc5bbe933d68c5be91b383b
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-05 11:39:47 +0000

    Exercise 39

---
+++ ex39.rb
@@ -0,0 +1,76 @@
+# A Hashmap or "hash" is a way to store data just like a list but instead of using only numbers
+# to get the data you can use almost anything. This lets you treat a hash like its a database for
+# storing and organizing data.
+
+# With hashes you can put new things in, not limited to strings for keys or values.
+# e.g stuff[1] = "wow" will put key 1 value "wow" into the hash.
+
+# You can delete from the hash using the .delete function
+# e.g stuff.delete(1) ie. delete pair associated with key '1'
+
+# create a mapping of state to abbreviation
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = "New York"
+cities['OR'] = "Portland"
+
+# puts out some cities
+puts '-' * 10
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
+
+# puts some states
+puts '-' * 10
+puts "Michigan's abbreviation is: #{states['Michigan']}"
+puts "Florida's abbreviation is: #{states['Florida']}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default ruby says 'nil' when something isn't in there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
